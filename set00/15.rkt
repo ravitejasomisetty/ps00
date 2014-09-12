052ea014f58ce86be1c90cc2d8f590746b0d0dd9
ravitejasomisetty <ravitejasomisetty8@gmail.com>
2014-09-12 16:14:07 -0400

    Exercise 15
    Write down reasonable comments for the definition of the type Student
    from Ex14 that define the types of the fields and their interpretation.

---
+++ set00/15.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |15|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")))))
+(define-struct student (id name major))
+; A student is a (make-student PosNumber String String)
+; Interpretation:
+; id = the unique id of the student
+; name = name of the student
+; major = the course major of the student
