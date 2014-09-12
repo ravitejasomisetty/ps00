b1d8582ca40ef54772f48bb266838ccaa115ba72
ravitejasomisetty <ravitejasomisetty8@gmail.com>
2014-09-12 15:32:13 -0400

    Exercise 12
    What are the values of the following expressions? Be sure to predict the
    answers in your head before trying the expressions.
    
    (make-point 5 3)
    (point? 5)
    (point? true)
    (point? (make-point 2 1))
    (point-x (make-point 8 5))
    (point-y (make-point 42 15))

---
+++ set00/12.rkt
@@ -0,0 +1,17 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |12|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")))))
+(define-struct point(x y))
+(make-point 5 3)
+(point? 5)
+(point? true)
+(point? (make-point 2 1))
+(point-x (make-point 8 5))
+(point-y (make-point 42 15))
+;(point? (make-point 5 2))
+(check-expect (make-point 5 3) (make-point 5 3))
+(check-expect (point? 5) false)
+(check-expect (point? true) false)
+(check-expect (point? (make-point 2 1)) true)
+(check-expect (point-x (make-point 8 5)) 8)
+(check-expect (point-y (make-point 42 15)) 15)
