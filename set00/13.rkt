397158c25054d9c47b399b9af3a33b4dfc5a12ab
ravitejasomisetty <ravitejasomisetty8@gmail.com>
2014-09-12 16:13:24 -0400

    Exercise 13
    What will happen if you type (make-point true false)? what is the result
    of (point-x (make-point true false))?

---
+++ set00/13.rkt
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |13|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")))))
+(define-struct point(x y))
+;A point is a (make-point any/c any/c)
+;x is interpreted as any information
+;y is interpreted as any information
+
+(check-expect (make-point true false) (make-point true false))
+(check-expect (point-x (make-point true false)) true)
