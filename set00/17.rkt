1078eefdad4116a9e39cf58b819432bf6c6e1bf4
ravitejasomisetty <ravitejasomisetty8@gmail.com>
2014-09-12 19:15:00 -0400

    Exercise 17
    Create some solid blue rectangles with the following dimensions:
    
    2x4
    4x8
    8x16
    16x32

---
+++ set00/17.rkt
@@ -0,0 +1,15 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |17|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")))))
+
+(rectangle  2 4 "solid" "blue")
+
+
+(rectangle  4 8 "solid" "blue")
+
+
+(rectangle  8 16 "solid" "blue")
+
+
+(rectangle  16 32 "solid" "blue")
+
